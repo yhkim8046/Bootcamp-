06ff1a91003e88eeee94bb1f8be79d1dfad96a5e
Yuhwan Kim <yhkim8046@gmail.com>
2024-12-10 20:08:12 +0900

    db added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const { Pool } = require('pg');
 const app = express();
+
 const PORT = 8080;
+
+//PostgreSql
+const pool = new Pool({
+    host: 'db',
+    user: 'postgres',
+    password:'8046',
+    database:'docker',
+    port:5432
+});
+
 
 app.get('/v1/api', (req, res)=>{
     res.json({message: 'server is running'});
@@ -9,3 +21,4 @@ app.get('/v1/api', (req, res)=>{
 app.listen(PORT, () => {
     console.log(`Server is running on Port: ${PORT}`); 
 });
+
